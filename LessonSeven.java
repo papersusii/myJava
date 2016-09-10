b87654173200b2624e44a6d40f2d8674e7430bbf
Ian A <ian.adler.nc@gmail.com>
2016-09-10 02:52:11 +0100

    removing the trash that I used to test

--- LessonSeven.java
+++
@@ -1,8 +0,0 @@
-public class LessonSeven{
-	public static void main(String[] args){
-		Monster Frank = new Monster();
-		Frank.name="Frank";
-		
-		System.out.println(Frank.name + "has an attack of " + Frank.getAttack);
-	}
-}
